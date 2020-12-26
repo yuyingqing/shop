d815df5bb35ea0d4eaf68cefc1b96a4a281ed2dc
lenovo <123456789 9@qq.com>
2020-12-26 17:14:33 +0800

    1、读取Excel的数据 2、对比键盘输入的数据

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Test {
@@ -10,5 +11,19 @@ public class Test {
         System.out.println("请输入密码：");
         String password=sc.next();
         System.out.println("你输入的密码为："+password);
+
+        File file=new File("C:\\Users\\lenovo\\IdeaProjects\\shop\\src\\users.xlsx");
+        ReadExcel readExcel=new ReadExcel();//创建对象
+        User user[]=readExcel.readExcel(file);
+        for(int i=0;i<user.length;i++)
+        {
+            if(username.equals(user[i].getUsername()) && password.equals(user[i].getPassword())){
+                System.out.println("登录成功");
+                break;
+            }
+            else{
+                System.out.println("登录失败");
+            }
+        }
     }
 }
